274726208a41f4d50457fef5af25278ea54decbd
theMistletoe <katachiwotodomete@gmail.com>
2022-02-21 23:07:25 +0900

    expressの記述直しつつdynamoDB操作できるように

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,14 @@
 import serverlessExpress from '@vendia/serverless-express';
 import express from "express";
-import { ScanInput, DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import { v4 as uuidv4 } from 'uuid';
+import bodyParser from 'body-parser';
 
 const app = express();
+const router = express.Router()
+
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
 
 const TODO_TABLE = process.env.TODO_TABLE;
 const dynamoDbClient = process.env.IS_OFFLINE ? new DocumentClient({
@@ -17,40 +22,47 @@ const dynamoDbClient = process.env.IS_OFFLINE ? new DocumentClient({
   // region: 'ap-northeast-1',
 })
 
-app.get("/", (_req, res, _next) => {
+router.get("/", (_req, res, _next) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
 });
 
-app.get("/hello", (_req, res, _next) => {
+router.get("/hello", (_req, res, _next) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
 });
 
-app.get("/todos", async (_req, res, _next) => {
-  const params: ScanInput = {
+router.get("/todos", async (req, res, _next) => {
+  const params: DocumentClient.QueryInput = {
     TableName: TODO_TABLE!,
+    KeyConditionExpression: "#uid = :uid",
+    ExpressionAttributeNames:{
+      "#uid": "userId"
+    },
+    ExpressionAttributeValues: {
+        ":uid": req.query.userid
+    }
   };
-  const response = await dynamoDbClient.scan(params).promise();
-  console.log(JSON.stringify(response));
+
+  const response = await dynamoDbClient.query(params).promise();
 
   return res.status(200).json({
     message: JSON.stringify(response),
   });
 });
 
-app.post("/todos", async (req, res, _next) => {
+router.post("/todos", async (req, res, _next) => {
   const putParams: DocumentClient.PutItemInput = {
     TableName: TODO_TABLE!,
     Item: {
-      id: uuidv4(),
+      userId: req.body.userid,
+      todoId: uuidv4(),
       title: req.body.title,
       description: req.body.description
     },
-    ReturnValues: 'ALL_OLD', // 同じプライマリキーの値があったら、過去の値を返す
-    // ReturnConsumedCapacity: "TOTAL",
+    ReturnValues: 'ALL_OLD',
   };
   await dynamoDbClient.put(putParams).promise();
 
@@ -59,10 +71,12 @@ app.post("/todos", async (req, res, _next) => {
   });
 });
 
-app.use((_req, res, _next) => {
+router.use((_req, res, _next) => {
   return res.status(404).json({
     error: "Not Found",
   });
 });
+
+app.use('/', router)
 
 export const handler = serverlessExpress({app});
